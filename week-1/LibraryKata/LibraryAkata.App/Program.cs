ff709668f9c51c4b08169af1fedcf5eabebf020a
MiguelAranda64 <0321101236@ut-tijuana.edu.mx>
2026-06-12 17:02:52 -0600

    day 3, we learned polymorphism and inheritance

---
+++ week-1/LibraryKata/LibraryAkata.App/Program.cs
@@ -0,0 +1,210 @@
+﻿// If I had code from another namespace I want to use here - I use a using statement
+using LibraryDomain;
+
+namespace LibraryKata.App; // A namespace is like a bucket or logical container for different
+// related code files.
+public class Program
+{
+    
+    // Now we are moving away from the Python file style Top-Level statements
+    // So we need a class to hold our Main() method. The previous style with no class
+    // or main - implicity had a Main() under the hood. 
+
+    // public - accessible across the program
+    // static - Main can be called upon without a Program object. It is a Static/class method. 
+    // void - it doesn't return anything
+    public static void Main()
+    {   
+        // When I call dotnet run, it finds Main() and begins code execution at the first line of the 
+        // main method. I wrote my code, inside DataTypesAndOperators() - a separate method. So if I want 
+        // that code to run, I need to call it inside Main()
+        Program.DataTypesAndOperators();
+        ClassesExample();
+        OopDemo();
+    }
+
+    // private - accessible only within this class
+    // static - it belongs to the class, not objects of the class
+    // void - returns nothing
+    private static void DataTypesAndOperators() // If I had arguments, or inputs for this method,
+    { // they would go inside the parenthesis after the method name 
+        Console.WriteLine("=== Data types and operators ==");
+
+        // C# is a Strongly typed language
+        // We cannot just create variables and shove whatever we want into them like JS or Python
+        int copies = 3; // whole numbers
+        double lateFee = 1; // floating point numbers (decimals)
+        bool isMember = true; // true/false values
+        char shelf = 'A'; // single character
+        string title = "Clean Code"; // text, strings are reference types
+
+        // Operators 
+        string user = "Jon"; // Single = is the assignment operator. 
+        int total = copies * 2; // example of an arithmetic operator, like + - * / 
+        bool isEnough = total > 4; // comparison - This line compares the value in total to 4, if it is greater
+        // than 4, isEnough will get 'true', otherwise it will get 'false'
+        // >, <, >=, <= - comparison operators
+        bool exactlySix = total == 6; // equality. Single equals is assignment, double equals is equality.
+        // unlike JS there is NO === all equality in C# is Strict equality
+        bool lendable = isMember && isEnough; //logical operators
+        // && - and, || - or, ! - reverses the condition that follows, ^ logical XOR - returns true if ONLY one condition is true
+
+        // This is the basic way to construct strings from other strings
+        // String concat - it works! But it can be messy
+        Console.WriteLine(title + " has been checked out by " + user);
+
+        // We can create much cleaner formatted strings
+        // using String Interpolation - a string with a $ before the opening quote
+        Console.WriteLine($"{title} on shelf {shelf}: {copies} copies, fee {lateFee}"); 
+
+        // C# has ALOT of shorthands and little shortcuts that you can find and use 
+        // to make your code easier to write. For example, lets say I want to add 1 to the value of total
+        // I could do something like
+        // total = total + 1; - ORRR
+        total += 1; // arithmetic shorthand for the same thing, also works for *= /= -=
+
+    }
+
+    private static void ControlFlow()
+    {
+        Console.WriteLine("\n== Control Flow ==");
+
+        // if - else if - else
+        int copiesAvailable = 0;
+        bool isMember = true;
+        if(copiesAvailable > 1 )
+        {
+            Console.WriteLine("Many available for checkout!");
+        }
+        else if (copiesAvailable == 1)
+        {
+            Console.WriteLine("Last copy!");
+        } else
+        {
+            Console.WriteLine("Out of stock!");    
+            Console.WriteLine("Check again later!");
+        }
+
+        // switch
+        string genre = "Mystery";
+        switch (genre)
+        {
+            case "Mystery":
+                Console.WriteLine("Check section A!");
+                break;
+            case "Science":
+                Console.WriteLine("Check section F!");
+                break;
+            default: // while optional, a default case to catch any edge cases is best practices
+                Console.WriteLine("Uh oh");
+                break;
+        }
+
+        // NEW in .NET 8, Switch expressions! You don't have to use these - they probably wont come up in QC
+        // but they're used out in real world code, so here is an example. In a switch expression, we want
+        // a return value from the switch - we can then use that value to print out a result
+
+        string section = genre switch
+        {
+            // This is my expression body
+            "Mystery" => "Section A",
+            "Science" => "Section F",
+            _ => "uh oh"
+        };
+        Console.WriteLine(section);
+        
+        }        
+        private static void Loops()
+        {
+        // C# provides for loops as well, 
+        // For, while, do-while, etc
+        for(int day = 1; day <= 3; day++)
+        {
+            Console.WriteLine($"Reminder day{day}: fee so far{calculateFee(day)}");
+        }
+
+        int onShelf = 3;
+        while(onShelf > 0)
+        {
+            Console.WriteLine($"{onShelf} copies on the shelf!");
+            onShelf--; // quick decrement shorthand
+        }
+        Console.WriteLine("No copies on shelf!");
+
+        string myString = "dog";
+
+        myString = "cat";
+    }
+
+    // I can use this for one line methods
+    private static decimal calculateFee(int daysLate) => daysLate *2;
+
+
+    private static void ArraysWork()
+    {
+        // C# provides for arrays as well as lists and other collections - we'll get to those later
+        string[] books = {"Dune","Happy Potter","Percy Jackson","Lord of the Rings"};
+        
+        Console.WriteLine(books[2]); // I can access individual elements - keeping in mind we index at 0
+        
+        // C# allows for for-each loops
+        foreach (string book in books)
+        {
+            Console.WriteLine(book);
+        }
+    }
+    private static void ClassesExample()
+    {
+        Console.WriteLine("using our Domain Book class");
+
+        // instantiating my first book, calling the constructor via "new" keyword
+        OldBook dune = new OldBook("Dune","Frank Herbert",3);
+        OldBook littlePrince = new OldBook("The Little Prince","Antoine",0);
+
+        // If I want to print book info, I can just pass the book variable
+        // It calls the toString() for me.
+        Console.WriteLine(dune);
+        Console.WriteLine(littlePrince.ToString());
+
+        Console.WriteLine($"Checking out Dune: {dune.checkOut()}"); // true
+        Console.WriteLine($"Checking out The Little Prince: {littlePrince.checkOut()}"); // false
+    }
+
+    public static void OopDemo()
+    {
+        Console.WriteLine("\n \n == OOP Demo stuff ==");
+
+        LibraryItem[] catalog =
+        {
+          new Book("Dune", "Frank Herbert", 2),
+          new ReferenceBook("C# Language Standards", "Microsoft", "Technology"),
+          new Magazine("Sports Illustrated", "Francisco", 5, "Conde Naste")
+        };
+
+        foreach(LibraryItem item in catalog)
+        {
+            Console.WriteLine(item.Describe());
+        }
+
+        // We can even use interfaces as reference types
+        foreach(LibraryItem item in catalog)
+        {
+            if (item is ILendable lendable)
+            {
+                Console.WriteLine($"{item.Title}: checkout -> {lendable.CheckOut()}");
+            } else
+            {
+                Console.WriteLine($"{item.Title} Reference only.");
+            }
+        }
+
+        // override vs new behaviour
+        Magazine wired = new Magazine("Wired", "Luis", 3, "Conde Nast");
+        LibraryItem baseMag = wired;
+
+        Console.WriteLine("== Override vs new on the same object, different ref type");
+        Console.WriteLine($"Magazine reference -> {wired.Describe()}");
+        Console.WriteLine($"LibraryItem reference -> {baseMag.Describe()}");
+    }
+}
+
---
+++ week-1/LibraryKata/LibraryDomain/Book.cs
@@ -0,0 +1,36 @@
+namespace LibraryDomain;
+
+public class Book : LibraryItem, ILendable
+{
+    // What is unique to a book (for our demo)
+    public int CopiesAvailable{get; private set;}
+
+    // Child class constructors look a little different
+    // We take in all our arguments for the parent + child, then call base with a colon
+    public Book(string title, string author, int copiesAvailable) : base(title, author)
+    {
+        CopiesAvailable = copiesAvailable;
+    }
+
+    // Because we have an abstract method in the parent, we MUST override it or we can't compile
+    public override string Describe()
+    {
+        return $"{Id}: {Title} by {Author} has {CopiesAvailable} copies available for checkout";
+    }
+
+    public bool CheckOut()
+    {
+        // Attempt to checkout a book - if copies is already 0, return false
+        if(CopiesAvailable == 0)
+            return false;
+
+            // Otherwise, we pass over the above code block
+            // We can decrement the available copies and return true
+            CopiesAvailable--;
+            return true;
+    }
+
+    // Providing for return behaviour
+    public void Return() => CopiesAvailable++;
+
+}
