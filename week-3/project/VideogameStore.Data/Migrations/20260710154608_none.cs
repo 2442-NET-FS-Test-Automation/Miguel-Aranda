1d9891c2f2ecf788a64faa916feab1310f8aa53a
MiguelAranda64 <0321101236@ut-tijuana.edu.mx>
2026-07-10 09:54:34 -0600

    new changes xd

---
+++ week-3/project/VideogameStore.Data/Migrations/20260710154608_none.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace VideogameStore.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class none : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "GameStore",
+                keyColumn: "Videogame_StoreId",
+                keyValue: 1,
+                column: "Stock",
+                value: 200);
+
+            migrationBuilder.UpdateData(
+                table: "GameStore",
+                keyColumn: "Videogame_StoreId",
+                keyValue: 2,
+                column: "Stock",
+                value: 120);
+
+            migrationBuilder.InsertData(
+                table: "GameStore",
+                columns: new[] { "Videogame_StoreId", "Stock", "StoreId", "VideogameId" },
+                values: new object[] { 3, 40, 2, 3 });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "GameStore",
+                keyColumn: "Videogame_StoreId",
+                keyValue: 3);
+
+            migrationBuilder.UpdateData(
+                table: "GameStore",
+                keyColumn: "Videogame_StoreId",
+                keyValue: 1,
+                column: "Stock",
+                value: 0);
+
+            migrationBuilder.UpdateData(
+                table: "GameStore",
+                keyColumn: "Videogame_StoreId",
+                keyValue: 2,
+                column: "Stock",
+                value: 0);
+        }
+    }
+}
